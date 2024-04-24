fa2d33d5c1d0f9a1165cc2a1260cffa0cce58d2c
Ankith <105031597+Ankith008@users.noreply.github.com>
2024-04-24 10:57:23 +0530

    server.js

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const connectDB = require("./db");
 const bodyParser = require("body-parser");
 
 connectDB();
-app.use(cors({ origin: "https://assig-client.vercel.app/" }));
+app.use(cors({ origin: "https://assig-client.vercel.app" }));
+app.options("*",cors())
 app.use(bodyParser.json());
 app.use("/auth", require("./routes/auth"));
 app.listen(5000);
